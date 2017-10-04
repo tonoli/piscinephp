819345e4a0b54c4d4b8282c6fc2668560762b3d8
Ignacio TONOLI DE MAUSSION <itonoli42@gmail.com>
2017-10-04 23:49:26 +0200

    last minute

--- d01/ex09/ssap2.php
+++ d01/ex09/ssap2.php
@@ -2,30 +2,33 @@
 <?php
 	$array = [];
 	$endArray = [];
-	foreach ($argv as $key => $elem)
-	{
-		if ($key != 0)
+	if ($argc > 1){
+		foreach ($argv as $key => $elem)
 		{
-			$array = array_filter(array_map('trim', explode(' ', $elem)));
-			foreach ($array as $key => $elem) {
-				array_push($endArray, $elem);
+			if ($key != 0)
+			{
+				$array = array_filter(array_map("trim", explode(' ', $elem)), 'strlen');
+				foreach ($array as $key => $elem) {
+					array_push($endArray, $elem);
+				}
+			}
+		}
+		sort($endArray, SORT_FLAG_CASE);
+		foreach ($endArray as $key => $elem) {
+			if(ctype_alpha($elem[0]) === true){
+				echo $elem."\n";
+			}
+		}
+		foreach ($endArray as $key => $elem) {
+			if(ctype_digit($elem[0]) === true){
+				echo $elem."\n";
+			}
+		}
+		foreach ($endArray as $key => $elem) {
+			if(!ctype_digit($elem[0]) && !ctype_alpha($elem[0])){
+				echo $elem."\n";
 			}
 		}
 	}
-	sort($endArray, SORT_FLAG_CASE);
-	foreach ($endArray as $key => $elem) {
-		if(ctype_alpha($elem[0]) === true){
-			echo $elem."\n";
-		}
-	}
-	foreach ($endArray as $key => $elem) {
-		if(ctype_digit($elem[0]) === true){
-			echo $elem."\n";
-		}
-	}
-	foreach ($endArray as $key => $elem) {
-		if(!ctype_digit($elem[0]) && !ctype_alpha($elem[0])){
-			echo $elem."\n";
-		}
-	}
+
 ?>
