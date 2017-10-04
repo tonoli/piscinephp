ab070451c20d6464ffd3551d6af7fa5101ee43cc
Ignacio TONOLI DE MAUSSION <itonoli42@gmail.com>
2017-10-04 22:23:49 +0200

    Prise en charge de le mot 0

--- d01/ex03/ft_split.php
+++ d01/ex03/ft_split.php
@@ -1,8 +1,7 @@
-#!/usr/bin/php
 <?php
 	function ft_split($arg)
 	{
-		$array = array_filter(array_map("trim", explode(' ', $arg)));
+		$array = array_filter(array_map("trim", explode(' ', $arg)), 'strlen');
 		sort($array);
 		return ($array);
 	}
